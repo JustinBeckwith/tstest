0e6ba1be7b265ec37c8906a168c95e4dd555d088
Justin Beckwith <beckwith@google.com>
2017-12-06 17:57:35 -0800

    initial commit

---
+++ src/test.js
@@ -0,0 +1,12 @@
+
+/**
+ * Add two numbers together.
+ * @param {number} num1
+ * @param {number} num2
+ * @returns {number} The sum of the two numbers.
+ */
+function doWork(num1, num2) {
+  return num1 + num2;
+}
+
+doWork(1, "NaN");
